8d3419ecc627380aac8ab732c292d3f7705869f5
sycuuui <102959791+sycuuui@users.noreply.github.com>
2022-11-27 01:33:34 +0900

    file edit

--- mqtt.py
+++ mqtt.py
@@ -1,16 +1,19 @@
+
 #publisher
 
 import time
 import paho.mqtt.client as mqtt
-import project
+import button
+import motor
+from datetime import datetime
 
 def on_connect(client,userdata,flag,rc):
-    client.subscribe("motor",qos=0)
+    client.subscribe("motor",qos=0) #topic=motor
 
 def on_message(client,userdata,msg):
     msg = int(msg.payload)
     print(msg)
-    project.controlMotor()
+    motor.controlMotor(msg)
 
 broker_ip = "localhost"
 
@@ -22,7 +25,13 @@ client.connect(broker_ip,1883)
 client.loop_start()
 
 while (True):
+    isBtn = button.driver()
+    if(isBtn==1):
+         now=datetime.now()
+         nowTime=now.strftime('%Y-%m-%d %H:%M:%S')
+         client.publish("driver",nowTime,qos=0)
     time.sleep(1)
+
 
 client.loop_stop()
 client.disconnect()
--- project.py
+++ project.py
@@ -1,5 +1,9 @@
 import time
 import RPi.GPIO as GPIO
+from motor import controlMotor
+from datetime import datetime
+now=datetime.now()
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
@@ -38,11 +42,10 @@ def ledOnOff(led,onOff):
 button1 = 21 #라즈베리쪽에 있는 것
 button2 = 12
 button3 = 25
-button4 = 23
 GPIO.setup(button1,GPIO.IN,GPIO.PUD_DOWN)
 GPIO.setup(button2,GPIO.IN,GPIO.PUD_DOWN)
 GPIO.setup(button3,GPIO.IN,GPIO.PUD_DOWN)
-GPIO.setup(button4,GPIO.IN,GPIO.PUD_DOWN)
+
 pwdArray=[] #비밀번호 입력받는 배열
 
 def changeStr(array): # 배열을 문자열로 바꿔줌
@@ -50,7 +53,23 @@ def changeStr(array): # 배열을 문자열로 바꿔줌
         return pwd
 
 errorCount = 0 #비밀번호를 잘못 입력 했을 때 count
-userPwd = 1212 #사용자의 비밀번호
+dict={}
+def pwd():
+        try:
+                file = open('./data/pwd.txt','r')
+        except:
+                return 
+        else:
+                for aline in file.readlines():
+                        list = aline.split(',')
+                        userNum=list[0]
+                        userPwd=list[1].strip()
+                        dict[userNum]=userPwd
+                        file.close()
+        return dict['1234']
+userPwd = pwd() #사용자의 비밀번호
+
+
 
 #모터
 moter = 18 #moter GPIO18에 연결
@@ -58,14 +77,57 @@ GPIO.setup(moter,GPIO.OUT)
 pwm = GPIO.PWM(moter,50)
 pwm.start(3.0)
 
-def controlMotor():
-        pwm.ChangeDutyCycle(7.5)
+#조도 센서
+def init(cs, mosi, miso, clk):
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(cs, GPIO.OUT)
+        GPIO.setup(mosi, GPIO.OUT)
+        GPIO.setup(miso, GPIO.IN)
+        GPIO.setup(clk,GPIO.OUT)
+
+def sendCmd(cs, mosi, clk, command):
+        GPIO.output(cs, GPIO.HIGH)
+        GPIO.output(cs, GPIO.LOW)
+        GPIO.output(clk, GPIO.LOW)
+        for i in range (4):
+                if(command & 0x80):
+                        GPIO.output(mosi, GPIO.HIGH)
+                else:
+                        GPIO.output(mosi, GPIO.LOW)
+                command = command << 1
+                GPIO.output(clk, GPIO.HIGH)
+                GPIO.output(clk, GPIO.LOW)
+
+def readData(miso, clk):
+        value = 0
+        for i in range(13):
+                GPIO.output(clk, GPIO.HIGH)
+                GPIO.output(clk, GPIO.LOW)
+                value = value << 1
+                if GPIO.input(miso):
+                        value = value | 0x1
+                else:
+                        value = value | 0x0
+        value = value >> 1
+        return value
+
+def getIlluminance():
+        cs = 8 # board.SPI_CE0_N
+        mosi = 10 # board.MOSI
+        miso = 9 # board.MISO
+        clk = 11 # board.CLK
+        init(cs, mosi, miso, clk)
+        sendCmd(cs, mosi, clk, ((0<<1)|0x0d)<<4)
+        return int(readData(miso, clk))
+
+mcpValue=getIlluminance()
 
 while True :
         distance = measureDistance(trig, echo)
-        pwm.ChangeDutyCycle(3.0) #0도로 설정
+        print(mcpValue)
         if(distance<=5):
-                print("택배상자가 들어왔습니다")
+                #print("택배상자가 들어왔습니다")
                 ledOnOff(led,ledOn)
                 ledOnOff(empty,ledOff)
         else:
@@ -75,7 +137,7 @@ while True :
         btnStatus1 = GPIO.input(button1)
         btnStatus2 = GPIO.input(button2)
         btnStatus3 = GPIO.input(button3)
-        btnStatus4 = GPIO.input(button4)
+        #btnStatus4 = GPIO.input(button4)
         if(btnStatus1==1): #1번 스위치를 누르면 배열에 1추가
                 pwdArray.append(1)
                 print(1)
@@ -85,23 +147,25 @@ while True :
         elif(btnStatus3==1): #3번 스위치를 누르면 배열에 3추가
                 pwdArray.append(3)
                 print(3)
-        elif(btnStatus4==1):
-                pwdArray.append(4)
-                print(4)       
+        #elif(btnStatus4==1):
+                #pwdArray.append(4)
+                #print(4)       
         if(len(pwdArray) == 4): #배열의 길이가 4일때
                 pwd=changeStr(pwdArray) #배열을 비교하기 위해서 문자열로 바꿈
                 if(str(userPwd)!=pwd): #사용자의 비밀번호와 입력받은 비밀번호가 다른 경우
-                        print(pwdArray)
                         print(changeStr(pwdArray))
                         pwdArray.clear() #배열 초기화
                         errorCount = errorCount +1 
                         print("다시 입력하세요") 
-                else: #사용자의 비밀번호와 입력받은 비밀번호가 같으면 무한루트 탈출
-                        pwm.ChangeDutyCycle(7.5) #비밀번호가 맞으t면 90도로 설정
+                else: 
+                        controlMotor(7.5) #비밀번호가 맞으t면 90도로 설정
                         time.sleep(1.0)
-                        print(pwdArray)
                         print(changeStr(pwdArray))
                         ledOnOff(error,ledOff)
-                if(errorCount%3==0): #errorCount가 3번 연속으로 틀린다면 노란색 LED켜짐(비밀번호를 제대로 입력 받을 때까지 켜놓음)
+                        break #조도 이상으로 잠시 해놓기
+                if(errorCount%3==0): #errorCount가 3번 연속으로 틀린다면 빨간색 LED켜짐(비밀번호를 제대로 입력 받을 때까지 켜놓음)
                                 ledOnOff(error,ledOn)
 
+        if(mcpValue<=500): # 조도 값이 500이하이면 모터로 잠금
+                time.sleep(2.0)
+                controlMotor(3.0)
